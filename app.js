390fcceb91a8ee743ffba550ede78b09429c30bc
Matvey <morozovmatvej75@gmail.com>
2023-05-26 20:04:15 +0300

    mousemove event

--- app.js
+++ app.js
@@ -4,12 +4,22 @@ const app = Vue.createApp({
       title: "the subtle art of not giving a fuck".toUpperCase(),
       author: "Mark Manson",
       rating: 10,
-      showBooks: true
+      showBooks: true,
+      x: 0,
+      y: 0
     }
   },
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
+    },
+    handleEvent(e, val) {
+      console.log(e);
+      if (val) console.log(val);
+    },
+    handleMousemove(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
     }
   }
 });
