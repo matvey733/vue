a6271b378111326d79a0b7c843d51a556b86ec4f
Matvey <morozovmatvej75@gmail.com>
2023-05-26 19:02:55 +0300

    init commit"

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      title: "the subtle art of not giving a fuck".toUpperCase(),
+      author: "Mark Manson",
+      rating: "10/10"
+    }
+  }
+});
+
+app.mount("#app");
