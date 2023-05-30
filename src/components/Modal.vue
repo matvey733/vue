ece6804fed54a95c96c29a7b8ded1626a513a8fc
Matvey <morozovmatvej75@gmail.com>
2023-05-30 16:55:19 +0300

    add global styles

---
+++ src/components/Modal.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="backdrop active">
+    <div class="modal">
+      <p>Vue isn't that bad actually</p>
+      <h1>modal title</h1>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+
+})
+</script>
+
+<style scoped>
+:root {
+  --transition-time: 300ms;
+}
+
+.modal {
+  min-width: 300px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, calc(-50% + 20px));
+  border: 2px solid black;
+  background-color: white;
+  color: black;
+  border-radius: 5px;
+  padding: 15px 30px;
+  transition: var(--transition-time);
+  opacity: 0;
+}
+
+.backdrop.active .modal {
+  opacity: 1;
+  transform: translate(-50%, calc(-50%));
+}
+
+.backdrop {
+  transition: var(--transition-time);
+  position: fixed;
+  top: 0;
+  left: 0;
+  pointer-events: none;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, .5);
+  opacity: 0;
+}
+
+.backdrop.active {
+  opacity: 1;
+}
+</style>
